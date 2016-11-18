8a8ed403a1880a6fb54c5fb09bece983ddb31a1d
Didier Kerinckx <Didi@Didiers-MacBook-Pro.local>
2016-11-18 10:54:43 +0100

    php artisan make:auth

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,9 +1,17 @@
-@extends('layouts.master')
+@extends('layouts.app')
 
 @section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading">Dashboard</div>
 
-    <div class="container">
-        <h1>Share2Camp</h1>
+                <div class="panel-body">
+                    You are logged in!
+                </div>
+            </div>
+        </div>
     </div>
-
+</div>
 @endsection
