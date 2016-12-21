6d8978acf77d4e445888b77f81c0797e53e82605
Didier Kerinckx <Didi@Didiers-MacBook-Pro.local>
2016-12-21 15:40:04 +0100

    style updates

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,36 +3,60 @@
 @section('content')
 <div class="intro">
 
-    <div class="container">
+
 
         <div class="introtext">
-            <h1>Welkom bij TravelShare</h1>
-            <h2>Zoek reismateriaal in uw buurt</h2>
+
+            <h1>Travel<span>Share</span></h1>
+            <h5>The cheapest way to discover the world</h5>
+
+
         </div>
 
+        <div class="searcharea">
 
-        <div class="row">
-        @foreach ($articles as $article)
+            <h3>Wat zoekt u ?</h3>
+            <form class="form-inline" action="">
+            <div class="form-group">
+                <input class="search_input" type="text">
+                <button type="submit" class="btn btn-default search_button" >Zoeken</button>
+            </div>
+            </form>
+        </div>
 
-                <div class="col-md-4 ">
-                    <h4>{{ $article->naam }}</h4>
-                    <h5>Prijs per dag : {{ $article->prijs }} €</h5>
-                    <h5>{{ $article->user_name }}</h5>
+</div>
+
+    <div class="container">
+
+        <div class="content">
+            <h1>Materiaal te huur</h1>
+
+            <div class="row">
+                @foreach ($articles as $article)
+
+                    <div class="col-md-4 ">
+                        <h4>{{ $article->naam }}</h4>
+                        <h5>Prijs per dag : {{ $article->prijs }} €</h5>
+                        <h5>{{ $article->user_name }}</h5>
 
 
-                    <div>
-                        <img class="project_upload" src="/uploads/articles/{{ $article->file_name1 }}">
+                        <div>
+                            <img class="project_upload" src="/uploads/articles/{{ $article->file_name1 }}">
+                        </div>
+
+                        <a href="/article/{{ $article->id }}"><button class="btn btn-primary details">Details</button></a>
                     </div>
 
-                    <a href="/article/{{ $article->id }}"><button class="btn btn-primary details">Details</button></a>
-                </div>
-
-        @endforeach
+                @endforeach
+            </div>
         </div>
+
+
+
     </div>
 
 
-</div>
+
 
 
 @endsection
