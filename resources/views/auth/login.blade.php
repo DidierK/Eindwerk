fbdec9eefdca355529e2a0d092039587200a14e7
Wout Borghgraef <wout.borghgraef@gmail.com>
2017-04-06 03:30:21 +0200

    organize sass files in folders

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,9 +1,11 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-<p>Om de veiligheid en betrouwbaarheid op TravelShare te garanderen, ben je verplicht je te registreren via Facebook.</p>
-<p>Je privacy instellingen kunnen worden aangepast in de instellingen van je profiel.</p>
-<button>Aanmelden met Facebook</button>
+<div class="hero">
+	<div class="container">
+		<p>Om de veiligheid en betrouwbaarheid op TravelShare te garanderen, ben je verplicht je te registreren via Facebook.</p>
+		<p>Je privacy instellingen kunnen worden aangepast in de instellingen van je profiel.</p>
+		<button>Aanmelden met Facebook</button>
+	</div>
 </div>
 @endsection
