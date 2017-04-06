f97d9e1a27905b1323d6c66c04f55de214970271
Wout Borghgraef <wout.borghgraef@gmail.com>
2017-04-06 13:51:46 +0200

    improve layout, text and button styling on login screen

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,10 +2,14 @@
 
 @section('content')
 <div class="hero">
-	<div class="container">
-		<p>Om de veiligheid en betrouwbaarheid op TravelShare te garanderen, ben je verplicht je te registreren via Facebook.</p>
-		<p>Je privacy instellingen kunnen worden aangepast in de instellingen van je profiel.</p>
-		<button>Aanmelden met Facebook</button>
+	<div class="login-portal container">
+		<div class="text--centered pt-10">
+			<div class="pb-2 text--light">
+				<p>Om de veiligheid en betrouwbaarheid op TravelShare te garanderen, ben je verplicht je te registreren via Facebook.</p>
+				<p>Je privacy instellingen kunnen worden aangepast in de instellingen van je profiel.</p>
+			</div>
+			<button class="button button--white button--facebook">Aanmelden met Facebook</button>
+		</div>
 	</div>
 </div>
 @endsection
