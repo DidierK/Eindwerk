b1ac72cec19d4abf640b3965d262db7e4e5c6ff9
Wout Borghgraef <wout.borghgraef@gmail.com>
2017-04-07 16:21:36 +0200

    Reworked and improved login screen

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,15 +1,49 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 
-@section('content')
-<div class="hero">
-	<div class="login-portal container">
-		<div class="text--centered pt-10">
-			<div class="pb-2 text--light">
-				<p>Om de veiligheid en betrouwbaarheid op TravelShare te garanderen, ben je verplicht je te registreren via Facebook.</p>
-				<p>Je privacy instellingen kunnen worden aangepast in de instellingen van je profiel.</p>
+	<!-- CSRF Token -->
+	<meta name="csrf-token" content="{{ csrf_token() }}">
+
+	<title>{{ config('app.name', 'Laravel') }}</title>
+
+	<!-- Styles -->
+	<link rel="stylesheet" href="{{ URL::asset('css/app.css') }}" />
+
+	<!-- Scripts -->
+	<script>
+		window.Laravel = <?php echo json_encode([
+			'csrfToken' => csrf_token(),
+			]); ?>
+		</script>
+	</head>
+	<body class="logged-out container">
+		<div id="app">
+			<div class="header">
+			<a href="#">
+				<img class="logo" src="{{ URL::asset('images/TravelShare_logo.png') }}">
+				</a>
 			</div>
-			<button class="button button--white button--facebook">Aanmelden met Facebook</button>
+			<div class="auth">
+				<p>Omdat wij de betrouwbaarheid en veiligheid van onze website willen garanderen laten we enkel toe in te loggen via Facebook. Privacy instellingen kunnen later nog worden aangepast</p>
+				<p>Voor meer info kan u terecht op <a href="#">onze gebruikersvoorwaarden</a>.</p>
+				<button class="button button--blue button--facebook">Aanmelden met Facebook</button>
+				<div class="back">
+				<a href="#">Ga terug naar de website</a>
+				</div>
+			</div>
+			<div class="footer">
+				<ul class="list-inline">
+					<li><a href="#">Gebruikersvoorwaarden</a></li>
+					<li><a href="#">Over TravelShare</a></li>
+					<li><a href="#">Contacteer ons</a></li>
+				</ul>
+			</div>
 		</div>
-	</div>
-</div>
-@endsection
+		<!-- Scripts -->
+		<script src="/js/app.js"></script>
+	</body>
+	</html>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,57 +1,40 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="intro">
-        <div class="introtext">
-            <h1>Travel<span>Share</span></h1>
-            <h5>The cheapest way to discover the world</h5>
-        </div>
-
-        <div class="searcharea">
-
-
-
-                {!! Form::open(array('url' => 'search', 'method' => 'POST', 'class' => 'form-inline')) !!}
-                    <label class="search_header">Wat zoekt u ?</label>
-                    <div class="form-group">
-                        <input name="keyword" class="search_input" type="text">
-                        <button type="submit" class="btn btn-default search_button" >Zoeken</button>
-                    </div>
-                {!! Form::close() !!}
-
-        </div>
-
+<div class="hero">
+    <div class="hero-content">
+        <h1 class="hero-title">Reismateriaal was nog nooit eenvoudiger</h1>
+        <div class="search-form">
+        {!! Form::open(['url' => 'foo/bar', 'class' => 'form-inline']) !!}
+        {!! Form::text('search-box', null, array('placeholder'=>'Wat hebt u nodig?')) !!}
+        {{Form::button('Zoeken', array('type' => 'submit', 'class' => ''))}}
+        {!! Form::close() !!}
+        </div> 
+    </div> 
 </div>
+<div class="container">
+    <div class="content">
+        <h1>Materiaal te huur</h1>
 
-    <div class="container">
+        <div class="row">
+            @foreach ($articles as $article)
 
-        <div class="content">
-            <h1>Materiaal te huur</h1>
+            <div class="col-md-3 home_articles ">
+                <div class="article_box">
 
-            <div class="row">
-                @foreach ($articles as $article)
+                    <p class="article_name">{{ $article->naam }}</p>
+                    <div>
+                        <a href="/article/{{ $article->id }}"><img class="project_upload" src="/uploads/articles/{{ $article->file_name1 }}"></a>
 
-                    <div class="col-md-3 home_articles ">
-                    <div class="article_box">
-
-                        <p class="article_name">{{ $article->naam }}</p>
-                        <div>
-                            <a href="/article/{{ $article->id }}"><img class="project_upload" src="/uploads/articles/{{ $article->file_name1 }}"></a>
-
-                        </div>
-                        <div class="article_info">
-                            <p>{{ $article->prijs }} € / dag</p><p>{{ $article->user_name }}</p>
-                        </div>
                     </div>
+                    <div class="article_info">
+                        <p>{{ $article->prijs }} € / dag</p><p>{{ $article->user_name }}</p>
                     </div>
-
-                @endforeach
+                </div>
             </div>
+
+            @endforeach
         </div>
-
     </div>
-
-
-
-
+</div>
 @endsection
