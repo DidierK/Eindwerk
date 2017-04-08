3407c761570667fd79cbde3420512d158da9354e
Wout Borghgraef <wout.borghgraef@gmail.com>
2017-04-08 21:30:23 +0200

    rename items view folder to users folder to match routes

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -13,7 +13,7 @@ class ItemController extends Controller
      */
     public function index()
     {
-        return view('items.index');
+        return view('user.index');
     }
 
     /**
---
+++ resources/views/user/transactions.php
@@ -0,0 +1,21 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="profile-hero">
+	<div class="container">
+		<h1 class="title">Transacties</h1>
+		<!-- Put a content tag here -->
+	</div>
+</div>
+<div class="user-nav">
+<div class="container">
+		<ul class="nav-items-container transaction-nav list-inline half-width-items">
+			<li class="active"><a href="#">Lopend</a></li>
+			<li><a class="" href="#">Geschiedenis</a></li>
+		</ul>
+		</div>
+</div>
+<div class="content">
+</div>
+</div>
+@endsection
