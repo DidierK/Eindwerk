54fc5f53e2c77ba6fe43ca39ec7758f3a7cc0d4f
Wout Borghgraef <wout.borghgraef@gmail.com>
2017-04-09 03:33:08 +0200

    add active tab for user navigation

---
+++ resources/assets/js/components/ProfileNav.vue
@@ -0,0 +1,21 @@
+<template>
+    <ul class="nav-items-container transaction-nav list-inline half-width-items">
+        <li v-on:click="activate"><a class="active" href="#">Mijn spullen</a></li>
+        <li v-on:click="activate"><a href="#">Instellingen</a></li>
+    </ul>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            // TODO MAKE OUR LINKS LINK TO A JS FUNCTION WHICH DYNAMICALLY LOADS IN A VIEW
+            // TODO: Check which url the loaded component is on and give that url active class
+        },
+        methods: {
+            activate: function(event){
+                $(".user-nav a").removeClass("active");
+                $(event.target).addClass("active");
+            }
+        }
+    }
+</script>
--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -8,12 +8,9 @@
 	</div>
 </div>
 <div class="user-nav">
-<div class="container">
-		<ul class="nav-items-container transaction-nav list-inline half-width-items">
-			<li class="active"><a href="#">Mijn spullen</a></li>
-			<li><a class="" href="#">Instellingen</a></li>
-		</ul>
-		</div>
+	<div class="container">
+		<profile-nav></profile-nav>
+	</div>
 </div>
 <div class="content">
 </div>
