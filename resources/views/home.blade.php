a87160394a34600543aa5b17d68b58bf9117095c
Didier Kerinckx <Didi@Didiers-MacBook-Pro.local>
2017-05-15 12:47:03 +0200

    style change

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
 	@include('partials.header')
 	<v-container class="Container">
 		<div class="u--textCenter">
-			<h1 class="Hero__heading">Huren op maat van jou bestemming</h1> 
+			<h1 class="Hero__heading">Huren op maat van jouw bestemming</h1>
 			<h2 class="Hero__subheading">Zoek naar alle dingen die jij nog mist voor een perfecte vakantie. <a href="#">Hier</a> leggen we uit hoe het werkt.</h2>
 		</div>
 		<v-search class="Search Search--main">
