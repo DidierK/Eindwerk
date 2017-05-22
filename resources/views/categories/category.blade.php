c788ce6d7a442b15a3023ecf4c51d5ea35edea86
Wout Borghgraef <wout.borghgraef@gmail.com>
2017-05-22 17:17:45 +0200

    improve styling category page

--- resources/views/categories/category.blade.php
+++ resources/views/categories/category.blade.php
@@ -2,25 +2,26 @@
 
 @section('content')
 <!-- Hero -->
-<v-hero class="Hero Hero--category" style="background: linear-gradient(rgba(0, 0, 0, 0.25), rgba(0, 0, 0, 0.25)), url(' {{ asset('images/category-heroes/' . $category->hero) }} ');background-position: 50% 50%; background-size: cover; background-repeat: no-repeat;">
 <v-container class="Container" v-cloak>
 
 	<!-- Header -->
-	<v-header class="Header Header--hero Header--dark">
+	<v-header class="Header Header--page">
 		<h1 class="Header__title">{{ ucwords($category->name) }}</h1>
 		<p>{{ $category->description }}</p>
 	</v-header>
 
 </v-container>
-</v-hero>
+
 
 <!-- Content -->
 <v-container class="Container" v-cloak>
+	<v-card class="Card">
 	@foreach ($items as $item)
 	<div>
 		<!-- nog de image toevoegen voor dit object -->
 		<v-link class="Link" link="{{ url('item/' . $item->url )}}">{{ $item->name }} </v-link>
 	</div>
 	@endforeach
+	</v-card>
 </v-container>
 @endsection
