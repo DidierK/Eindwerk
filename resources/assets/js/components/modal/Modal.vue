87af0c2d8721c766b6e5c370d3c6af1137464bed
Wout Borghgraef <wout.borghgraef@gmail.com>
2017-05-30 19:09:09 +0200

    improve styling global modal

--- resources/assets/js/components/modal/Modal.vue
+++ resources/assets/js/components/modal/Modal.vue
@@ -1,26 +1,41 @@
 <style>
+	.Modal {
+		position: relative;
+		/* Only put on parent modal a z-index so it gets shown properly */
+		z-index: 2;
+	}
 
 	.Modal .Modal__overlay {
 		background-color: rgba(0, 0, 0, 0.5);
-		position: absolute;
-		top: 0px;
-		left: 0px;
+		position: fixed; /* This way it will always cover all area */
+		top: 0;
+		left: 0;
 		width: 100%;
 		height: 100%;
-		z-index: 1;
 	}
 
 	.Modal .Modal__content {
 		background-color: #FFF;
 		box-shadow: 0 2px 6px 0 rgba(0,0,0,.44);
-		padding: 32px;
+		box-sizing: border-box;
+		overflow-y: auto;
+		padding: 16px;
 		position: fixed;
 		top: 50%;
 		left: 50%;
 		transform: translate(-50%, -50%);
-		width: 100%;
-		max-width: 520px;
-		z-index: 2;
+		width: 90%;
+		max-width: 640px;
+		max-height: 360px;
+	}
+
+	@media screen and (min-width: 520px) {
+		.Modal .Modal__content {
+			max-height: 100%;
+			padding: 32px;
+			transform: translate(-50%, -50%);
+			left: 50%;
+		}
 	}
 
 	.Modal-enter-active, .Modal-leave-active {
