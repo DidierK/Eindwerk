57619865765d74cd5b2711e2f78622150ef2d781
Wout Borghgraef <wout.borghgraef@gmail.com>
2017-06-16 23:21:28 +0200

    styled errors on add item moda

--- resources/assets/js/components/form/AddItemForm.vue
+++ resources/assets/js/components/form/AddItemForm.vue
@@ -1,13 +1,26 @@
 <style>
+	.Errors {
+		background-color: #ea4031;
+		border-radius: 5px;
+		color: #fff;
+		padding: 16px;
+	}
+
+	.Errors h3 {
+		color: #fff;
+		margin-bottom: .5em;
+	}
 </style>
 <template>
 	<form class="Form Form--item-creation" :action="action" :method="method" enctype="multipart/form-data" v-on:submit.prevent="addItem" v-else>
-		<div class="Errors">
+		<div class="Errors" v-if="showErrors">
+			<h3>Sommige velden zijn incorrect</h3>
 			<p v-for="error in errors">{{ error[0] }}</p>
 		</div>
 		<slot name="csrf"></slot>
 		<v-form-item class="FormItem">
 			<select v-model="formData.itemName" name="item_names" class="input--full-width">
+				<option disabled value="">Selecteer een item</option>
 				<v-option v-for="itemName in data">{{ itemName.label }}</v-option>
 			</select>
 			<p class="u--textSmall u--mt-8">Staat uw item er nog niet tussen? <a href="#">Laat het ons weten en doe een suggestie!</a></p>
@@ -44,6 +57,7 @@
 					description: ''
 				},
 				errors: [],
+				showErrors: false,
 				showSpinner: false
 			}
 		},
@@ -52,9 +66,11 @@
 				this.showSpinner = true;
 				axios.post('/user-item', this.fillForm()).then((response) => {
 					if(response.data.length === 0 ) {
+						this.showErrors = false;
 						window.location.href = "/profile/my-items";
 					} else {
 						this.errors = response.data;
+						this.showErrors = true;
 					}
 
 					this.showSpinner = false;
