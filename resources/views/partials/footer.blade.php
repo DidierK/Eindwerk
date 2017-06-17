66c1a2c689872509fcf39fe3ee8ab9ee9fcdbf56
Wout Borghgraef <wout.borghgraef@gmail.com>
2017-06-17 17:08:07 +0200

    remove link to add items

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,7 +1,7 @@
  <v-footer class="Footer Footer--main u--flex u--flexJustifyContentSpaceBetween u--posRelative" v-cloak>
 
-    <v-facebooklogo class="facebook"><a target="blank" href="https://www.facebook.com/TravelShareMechelen/"></a></v-facebooklogo>
-     <v-copyright class="copyright">Copyright © 2017 <br> TravelShare All Rights Reserved</v-copyright>
+    <div class="facebook"><a target="blank" href="https://www.facebook.com/TravelShareMechelen/"></a></div>
+     <div class="copyright">Copyright © 2017 <br> TravelShare All Rights Reserved</div>
      <!--
  	<v-ul class="List--footer-nav u--flex">
  		<v-li v-cloak><a href="#" class="u--linkClean">Gebruikersvoorwaarden</a></v-li>
