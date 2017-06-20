11e9a59e7ea77f324f311d92a74330511bc351f3
Bram De Nyn <bramdenyn_94@hotmail.com>
2017-06-20 21:21:02 +0200

    mobile team photos

--- resources/assets/js/components/list/UnorderedList.vue
+++ resources/assets/js/components/list/UnorderedList.vue
@@ -15,6 +15,18 @@
     text-align: center;
   }
 
+	.List--team{
+		display: flex;
+		flex-direction: column;
+		align-content: center;
+	}
+
+	.List--team li{
+		margin: 20px 0 20px 0;
+	}
+
+
+
   .List--team .ImageContainer,
   .List--how-it-works .ImageContainer {
     margin: 0 auto;
@@ -45,6 +57,8 @@
     width: 100%;
   }
 
+
+
   @media (min-width: 420px){
     .List--user-items > .List__item  {
       width: calc(50% - 8px);
@@ -58,6 +72,15 @@
       flex: 0 0 33.33%;
       box-sizing: border-box;
     }
+
+		.List--team{
+			flex-direction: row;
+			justify-content: space-around;
+		}
+
+		.List--team li{
+			margin: 0px 0 0px 0;
+		}
   }
 
   @media (min-width: 1200px){
