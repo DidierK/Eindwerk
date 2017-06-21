cf7ca33d481482b69c23bfbebb1215f563ab374f
Wout Borghgraef <wout.borghgraef@gmail.com>
2017-06-21 03:03:38 +0200

    add transactions table

--- database/migrations/2017_06_21_005657_create_transactions_table.php
+++ database/migrations/2017_06_21_005657_create_transactions_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateRequestsTable extends Migration
+class CreateTransactionsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -12,19 +12,18 @@ class CreateRequestsTable extends Migration
      * @return void
      */
     public function up() {
-        Schema::create('requests', function (Blueprint $table) {
+        Schema::create('transactions', function (Blueprint $table) {
             $table->increments('id');
-            $table->integer('sender_id')->unsigned();
-            $table->foreign('sender_id')->references('id')->on('users')->onDelete('cascade');
-            $table->integer('receiver_id')->unsigned();
-            $table->foreign('receiver_id')->references('id')->on('users')->onDelete('cascade');
+            $table->integer('renter_id')->unsigned();
+            $table->foreign('renter_id')->references('id')->on('users')->onDelete('cascade');
+            $table->integer('owner_id')->unsigned();
+            $table->foreign('owner_id')->references('id')->on('users')->onDelete('cascade');
             $table->integer('user_item_id')->unsigned();
             $table->foreign('user_item_id')->references('id')->on('user_items')->onDelete('cascade');
             $table->date('start_date'); 
             $table->date('end_date');
-            $table->string('status')->default('Afwachten');    
+            $table->string('status')->default('Wachten op betaling');    
         });
-        //
     }
 
     /**
